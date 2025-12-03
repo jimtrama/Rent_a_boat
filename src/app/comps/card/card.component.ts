393bfa70e180da2f96bac6ce48b7c9349b3bcb07
Dimitrios <dimeragon@gmail.com>
2025-12-03 20:27:10 +0200

    with swip

--- src/app/comps/card/card.component.ts
+++ src/app/comps/card/card.component.ts
@@ -5,51 +5,94 @@ import { Boat } from '../../models/boat.model';
   selector: 'app-card',
   standalone: false,
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
 export class CardComponent {
-  @Input() boat!:Boat;
-  @Input() boatId!:number;
-  @Output() imgClicked:EventEmitter<string> = new EventEmitter();
-
+  @Input() boat!: Boat;
+  @Input() boatId!: number;
+  @Output() imgClicked: EventEmitter<string> = new EventEmitter();
 
   public selectedImg = 0;
 
   ngAfterViewInit(): void {
-    setTimeout(()=>{this.calculateImages();});
+    setTimeout(() => {
+      this.calculateImages();
+    });
   }
 
-  private calculateImages(){
-    let imgs = document.getElementsByClassName(this.boatId +'-img');
+  private calculateImages() {
+    let imgs = document.getElementsByClassName(this.boatId + '-img');
     //console.log(imgs);
-    
-    for (let i = this.selectedImg ; i<imgs.length;i++ ) {
-      (imgs[i] as HTMLElement ).style.left = (i*10)+"px"; 
-      (imgs[i] as HTMLElement ).style.height = (90-((i-this.selectedImg)*8))+"%"; 
-      (imgs[i] as HTMLElement ).style.zIndex = (imgs.length - i)+""; 
-      (imgs[i] as HTMLElement ).style.transform = "rotate("+((i-this.selectedImg)*2)+"deg)";
+
+    for (let i = this.selectedImg; i < imgs.length; i++) {
+      (imgs[i] as HTMLElement).style.left = i * 10 + 'px';
+      (imgs[i] as HTMLElement).style.height =
+        90 - (i - this.selectedImg) * 8 + '%';
+      (imgs[i] as HTMLElement).style.zIndex = imgs.length - i + '';
+      (imgs[i] as HTMLElement).style.transform =
+        'rotate(' + (i - this.selectedImg) * 2 + 'deg)';
     }
 
-    for (let i = this.selectedImg-1 ; i>=0;i-- ) {
-      (imgs[i] as HTMLElement ).style.left = (i*10)+"px"; 
-      (imgs[i] as HTMLElement ).style.height = (90-(i*5))+"%"; 
-      (imgs[i] as HTMLElement ).style.zIndex = (i)+""; 
-      (imgs[i] as HTMLElement ).style.transform = "rotate("+((i-this.selectedImg)*2)+"deg)";
+    for (let i = this.selectedImg - 1; i >= 0; i--) {
+      (imgs[i] as HTMLElement).style.left = i * 10 + 'px';
+      (imgs[i] as HTMLElement).style.height = 90 - i * 5 + '%';
+      (imgs[i] as HTMLElement).style.zIndex = i + '';
+      (imgs[i] as HTMLElement).style.transform =
+        'rotate(' + (i - this.selectedImg) * 2 + 'deg)';
     }
   }
 
-  onImgClick(img:string){
+  onImgClick(img: string) {
     console.log(img);
     this.imgClicked.emit(img);
   }
 
-  onRightArrowClick(){
+  onRightArrowClick() {
     this.selectedImg++;
     this.calculateImages();
   }
 
-  onLeftArrowClick(){
+  onLeftArrowClick() {
     this.selectedImg--;
     this.calculateImages();
+  }
+
+  private imageSwinging = false;
+  private xTouchStart = 0;
+  onTouchStartImg(e: TouchEvent) {
+    console.log(e);
+    this.imageSwinging = true;
+    this.xTouchStart = e.targetTouches[0].pageX;
+  }
+
+  onTouchMove(e: TouchEvent) {
+    let xDiff = 0;
+    if (this.imageSwinging) {
+      e.preventDefault();
+      let imgs = document.getElementsByClassName(this.boatId + '-img');
+      xDiff = e.targetTouches[0].pageX - this.xTouchStart;
+      console.log(xDiff);
+      if(xDiff>0 && this.selectedImg == 0) return;
+      if(xDiff<0 && this.selectedImg == imgs.length-1) return;
+      (imgs[this.selectedImg] as HTMLElement).style.left = xDiff + 'px';
+    
+    }
+
+    if (xDiff < -140 && this.imageSwinging) {
+      this.selectedImg++;
+      this.calculateImages();
+      this.imageSwinging = false;
+    }
+
+    if (xDiff > 140 && this.imageSwinging) {
+      this.selectedImg--;
+      this.calculateImages();
+      this.imageSwinging = false;
+    }
+  }
+
+  onTouchEnd(e:TouchEvent){
+    let imgs = document.getElementsByClassName(this.boatId + '-img');
+      (imgs[this.selectedImg] as HTMLElement).style.left = 0 + 'px';
   }
 }
